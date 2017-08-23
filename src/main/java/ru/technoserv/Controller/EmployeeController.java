c80ae7fcab0430a21324079d323c1c82c5195d87
dmitry <witerium@gmail.com>
2017-08-23 17:49:55 +0300

    Добавлен поиск сотрудника по фамилии, которая передается как часть url

--- src/main/java/ru/technoserv/Controller/EmployeeController.java
+++ src/main/java/ru/technoserv/Controller/EmployeeController.java
@@ -1,10 +1,13 @@
 package ru.technoserv.Controller;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.logging.Logger;
 
 /**
  * Данный контроллер предназначен для обработки запросов на работу с
@@ -14,8 +17,8 @@ import javax.servlet.http.HttpServletRequest;
 @RestController
 public class EmployeeController {
 
-
-    /**
+    private static Logger log = Logger.getLogger(EmployeeController.class.getName());
+       /**
      * Запрос предназначен для создания работника
      * @param firstName имя
      * @param lastName фамилия
@@ -26,17 +29,30 @@ public class EmployeeController {
      * @param jobTitle должность
      * @return Сотрудника, который посылается клиенту в виде JSON объекта
      */
-    @RequestMapping("/employee")
-    public Employee employee(
+    @RequestMapping("/createEmployee")
+    public Employee createEmployee(
             @RequestParam(value="firstName") String firstName,
             @RequestParam(value="lastName") String lastName,
             @RequestParam(value="fatherName") String fatherName,
             @RequestParam(value="gender") String gender,
             @RequestParam(value="birthDate") String birthDate,
             @RequestParam(value="Salary") String salary,
-            @RequestParam(value = "jobTitle") String jobTitle
+            @RequestParam(value="jobTitle") String jobTitle,
+            HttpServletRequest request
     ){
+        log.info("User"+request.getRemoteAddr()+" are send "+request.getRemoteUser());
         return new Employee(firstName, lastName);
+    }
+
+    @RequestMapping("/employee/{employeeLastName}")
+    public Employee getEmployeeByName(
+            @PathVariable("employeeLastName") String lastName,
+            Model model,
+            HttpServletRequest request){
+                log.info("User "+request.getRemoteAddr()+" try found "+lastName);
+                //TODO выдавать нужного сотрудника вместо заглушки
+                return new Employee("Nanami", "Halozy");
+
     }
 
 }
