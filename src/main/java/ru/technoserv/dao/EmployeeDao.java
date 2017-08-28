4a4c1bb1b644ab3f82ec47fc385c112b24ba8b3e
oprimolenny <oprimolenny@tsconsulting.ru>
2017-08-28 10:52:12 +0300

    Изменен interface EmployeeDao. Методы read и delete теперь принимают (String firstname,String lastname) в качестве аргументов.

--- src/main/java/ru/technoserv/dao/EmployeeDao.java
+++ src/main/java/ru/technoserv/dao/EmployeeDao.java
@@ -2,7 +2,7 @@ package ru.technoserv.dao;
 
 public interface EmployeeDao {
     void create(Employee employee);
-    Employee read(int empID);
-    void delete(int empID);
+    Employee read(String firstName, String lastName);
+    void delete(String firstName, String lastName);
 }
 
