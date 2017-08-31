48c345ab70e59e21a15ef38eeb4a1d3e5917d2f0
oprimolenny <oprimolenny@tsconsulting.ru>
2017-08-31 14:21:44 +0300

    Изменен class EmployeeRowMapper Метод mapRow изменен и теперь задает все свойства обьекту типа Employee

--- src/main/java/ru/technoserv/dao/EmployeeRowMapper.java
+++ src/main/java/ru/technoserv/dao/EmployeeRowMapper.java
@@ -1,5 +1,6 @@
 package ru.technoserv.dao;
 
+import java.math.BigDecimal;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import org.springframework.jdbc.core.RowMapper;
@@ -8,8 +9,16 @@ public class EmployeeRowMapper implements RowMapper {
 
     public Object mapRow(ResultSet resultSet, int i) throws SQLException {
         Employee employee = new Employee();
-        employee.setFirstName(resultSet.getString("FIRST_NAME"));
-        employee.setLastName(resultSet.getString("LAST_NAME"));
+        employee.setEmpID((Integer) resultSet.getObject("EMP_ID"));
+        employee.setPosition((String) resultSet.getObject("TITLE"));
+        employee.setGrade((String) resultSet.getObject("DESCRIPTION"));
+        employee.setDepartment((String) resultSet.getObject("DEPT_NAME"));
+        employee.setFirstName((String) resultSet.getObject("FIRST_NAME"));
+        employee.setLastName((String) resultSet.getObject("LAST_NAME"));
+        employee.setPatrName((String) resultSet.getObject("PATR_NAME"));
+        employee.setGender((Character) resultSet.getObject("GENDER"));
+        employee.setBirthday(resultSet.getDate("BIRTHDAY"));
+        employee.setSalary((BigDecimal) resultSet.getObject("SALARY") );
         return employee;
     }
 }
