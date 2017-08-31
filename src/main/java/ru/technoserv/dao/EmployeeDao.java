d9a7ffaf4bac47c06adf2dd93f6540edb42c1253
oprimolenny <oprimolenny@tsconsulting.ru>
2017-08-31 11:08:49 +0300

    Произведен рефакторинг interface EmployeeDao

--- src/main/java/ru/technoserv/dao/EmployeeDao.java
+++ src/main/java/ru/technoserv/dao/EmployeeDao.java
@@ -4,11 +4,20 @@ import java.math.BigDecimal;
 
 public interface EmployeeDao {
     void create(Employee employee);
+
     Employee read(String firstName, String lastName);
+
     void delete(String firstName, String lastName);
+
     void deleteAllFromDept(Department dept);
+
     void updateDept(Employee emp, Department dept);
+
     void updatePsoition(Employee emp, String newPosition);
+
     void updateGrade(Employee emp, String newGrade);
+
     void updateSalary(Employee emp, BigDecimal newSalary);
+
+}
 
