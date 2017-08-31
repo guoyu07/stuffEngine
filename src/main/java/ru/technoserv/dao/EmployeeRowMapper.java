abe90463812ece65d3e18336e577d84fda3286c4
oprimolenny <oprimolenny@tsconsulting.ru>
2017-08-31 16:44:26 +0300

    Изменен class EmployeeRowMapper Исправлен метод mapRow(ResultSet resultSet, int i)

--- src/main/java/ru/technoserv/dao/EmployeeRowMapper.java
+++ src/main/java/ru/technoserv/dao/EmployeeRowMapper.java
@@ -9,14 +9,14 @@ public class EmployeeRowMapper implements RowMapper {
 
     public Object mapRow(ResultSet resultSet, int i) throws SQLException {
         Employee employee = new Employee();
-        employee.setEmpID((Integer) resultSet.getObject("EMP_ID"));
+        employee.setEmpID((Integer) resultSet.getInt("EMP_ID"));
         employee.setPosition((String) resultSet.getObject("TITLE"));
         employee.setGrade((String) resultSet.getObject("DESCRIPTION"));
         employee.setDepartment((String) resultSet.getObject("DEPT_NAME"));
         employee.setFirstName((String) resultSet.getObject("FIRST_NAME"));
         employee.setLastName((String) resultSet.getObject("LAST_NAME"));
         employee.setPatrName((String) resultSet.getObject("PATR_NAME"));
-        employee.setGender((Character) resultSet.getObject("GENDER"));
+        employee.setGender(resultSet.getString("GENDER").toCharArray()[0]);
         employee.setBirthday(resultSet.getDate("BIRTHDAY"));
         employee.setSalary((BigDecimal) resultSet.getObject("SALARY") );
         return employee;
