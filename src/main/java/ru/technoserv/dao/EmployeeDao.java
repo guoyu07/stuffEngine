9e2ddbf9aa6ee94431ce4a8d4ceda0951bb7aaab
oprimolenny <oprimolenny@tsconsulting.ru>
2017-08-31 11:06:58 +0300

    Изменен interface EmployeeDao Добавлены методы deleteAllFromDept, updateDept, updatePosition, updateGrade, updateSalary

--- src/main/java/ru/technoserv/dao/EmployeeDao.java
+++ src/main/java/ru/technoserv/dao/EmployeeDao.java
@@ -1,8 +1,14 @@
 package ru.technoserv.dao;
 
+import java.math.BigDecimal;
+
 public interface EmployeeDao {
     void create(Employee employee);
     Employee read(String firstName, String lastName);
     void delete(String firstName, String lastName);
-}
+    void deleteAllFromDept(Department dept);
+    void updateDept(Employee emp, Department dept);
+    void updatePsoition(Employee emp, String newPosition);
+    void updateGrade(Employee emp, String newGrade);
+    void updateSalary(Employee emp, BigDecimal newSalary);
 
