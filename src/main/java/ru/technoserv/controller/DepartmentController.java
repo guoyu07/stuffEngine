5700058fee5308630462a33a9159fd68022e67a8
amayindy <amayindy@tsconsulting.ru>
2017-09-01 15:02:21 +0300

    Добавлен контроллер DepartmentController, предназначенный для обработки запросов, связанных с отделами

---
+++ src/main/java/ru/technoserv/controller/DepartmentController.java
@@ -0,0 +1,17 @@
+package ru.technoserv.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Данный контроллер предназначен для обработки запросов, связанных
+ * с отделами.
+ */
+
+@RestController
+@RequestMapping("/department")
+public class DepartmentController {
+
+
+
+}
