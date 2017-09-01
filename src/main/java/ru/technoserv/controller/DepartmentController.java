b354e9223ad4ddb62af971938896f4f559bcdbba
amayindy <amayindy@tsconsulting.ru>
2017-09-01 15:52:48 +0300

    В DepartmentController добавлены методы (пока с пустыми телами) для: получения списка дочерних отделов, создания отдела, закрытия отдела, переподчинения отдела другому отделу

--- src/main/java/ru/technoserv/controller/DepartmentController.java
+++ src/main/java/ru/technoserv/controller/DepartmentController.java
@@ -1,7 +1,10 @@
 package ru.technoserv.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.technoserv.dao.Department;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Данный контроллер предназначен для обработки запросов, связанных
@@ -9,9 +12,29 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
-@RequestMapping("/department")
+@RequestMapping(value = "/department")
 public class DepartmentController {
 
+    @RequestMapping(value = "/{depId}/descendents", method = RequestMethod.GET)
+    public List<Department> getDescendents(@PathVariable Long depId) {
+        List<Department> descendents = new ArrayList<>();
 
+        return descendents;
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public void createDepartment(@RequestBody Department department) {
+
+    }
+
+    @RequestMapping(value = "/{depId}", method = RequestMethod.DELETE)
+    public void closeDepartment(@PathVariable Long depId) {
+
+    }
+
+    @RequestMapping(value = "/{depId}", method = RequestMethod.PATCH)
+    public void changeParentDept(@RequestBody Department department) {
+
+    }
 
 }
