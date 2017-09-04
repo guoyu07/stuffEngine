33f70cd699500244050a28392455c8417327d7bd
oprimolenny <oprimolenny@tsconsulting.ru>
2017-09-04 12:08:20 +0300

    добавлен class DepartmentServiceImpl

---
+++ src/main/java/ru/technoserv/services/DepartmentServiceImpl.java
@@ -0,0 +1,46 @@
+package ru.technoserv.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+import ru.technoserv.dao.Department;
+import ru.technoserv.dao.DepartmentDao;
+import ru.technoserv.dao.EmployeeDao;
+
+import java.util.List;
+
+@Component
+@ComponentScan("ru")
+public class DepartmentServiceImpl implements DepartmentService {
+
+    @Autowired
+    DepartmentDao departmentDao;
+    @Autowired
+    EmployeeDao employeeDao;
+    private boolean isLoaded;
+
+    @Override
+    public Department createDepartment(Department department) {
+        return null;
+    }
+
+    @Override
+    public List<Department> getAllDepartments() {
+        return null;
+    }
+
+    @Override
+    public List<Department> getSubDepts(int deptId) {
+        return null;
+    }
+
+    @Override
+    public void reassignDepartment(int dept, int newParentDept) {
+
+    }
+
+    @Override
+    public Department deleteDepartment(Department department) {
+        return null;
+    }
+}
