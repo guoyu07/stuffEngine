4b78ef6eba49192067d0c80fb74a94f473a072a5
AMaiyndy <aratmay@gmail.com>
2017-09-07 11:03:22 +0300

    Во все аннотации @RequestMapping методов в DepartmentController добавлен параметр name(описание действия); в параметры методов добавлен HttpServlet request request

--- src/main/java/ru/technoserv/controller/DepartmentController.java
+++ src/main/java/ru/technoserv/controller/DepartmentController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.*;
 import ru.technoserv.dao.Department;
 import ru.technoserv.services.DepartmentService;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 /**
@@ -19,32 +20,37 @@ public class DepartmentController {
     @Autowired
     DepartmentService departmentService;
 
-    @RequestMapping(value = "/{depId}/subdepts", method = RequestMethod.GET)
-    public List<Department> getSubDepts(@PathVariable Integer depId) {
+    @RequestMapping(value = "/{depId}/subdepts", method = RequestMethod.GET,
+            name = "получение дочерних отделов")
+    public List<Department> getSubDepts(@PathVariable Integer depId, HttpServletRequest request) {
         List<Department> subDepts = departmentService.getSubDepts(depId);
 
         return subDepts;
     }
 
-    @RequestMapping(value = "/{depId}", method = RequestMethod.GET)
-    public Department getDept(@PathVariable Integer depId) {
+    @RequestMapping(value = "/{depId}", method = RequestMethod.GET,
+            name = "поиск отдела по id")
+    public Department getDept(@PathVariable Integer depId, HttpServletRequest request) {
         return departmentService.getDepartment(depId);
     }
 
     @RequestMapping(value = "/", method = RequestMethod.POST,
-            produces = "application/json", consumes = "application/json")
-    public Department createDepartment(@RequestBody Department department) {
+            produces = "application/json", consumes = "application/json",
+            name = "создание отдела")
+    public Department createDepartment(@RequestBody Department department, HttpServletRequest request) {
         return departmentService.createDepartment(department);
     }
 
-    @RequestMapping(value = "/{depId}", method = RequestMethod.DELETE)
-    public Department closeDepartment(@PathVariable Integer depId) {
+    @RequestMapping(value = "/{depId}", method = RequestMethod.DELETE,
+            name = "удаление отдела")
+    public Department closeDepartment(@PathVariable Integer depId, HttpServletRequest request) {
         return departmentService.deleteDepartment(depId);
     }
 
-    @RequestMapping(value = "/{depId}", method = RequestMethod.PATCH)
+    @RequestMapping(value = "/{depId}", method = RequestMethod.PATCH,
+            name = "переподчинение отдела другому отделу")
     public void changeParentDept(@RequestBody Department newParentDepartment,
-                                 @PathVariable Integer depId) {
+                                 @PathVariable Integer depId, HttpServletRequest request) {
         departmentService.reassignDepartment(depId, newParentDepartment.getId());
     }
 
