5064599b13712cb274d2f1a7c5ae06f4a3026ffb
dmitry <witerium@gmail.com>
2017-09-14 13:00:13 +0300

    Исправлен формат даты на dd.mm.yyyy

--- src/main/java/ru/technoserv/controller/GsonUtility.java
+++ src/main/java/ru/technoserv/controller/GsonUtility.java
@@ -3,9 +3,11 @@ package ru.technoserv.controller;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.text.DateFormat;
+
 public class GsonUtility {
 
-    private static Gson gson = new GsonBuilder().serializeNulls().create();
+    private static Gson gson = new GsonBuilder().serializeNulls().setDateFormat("dd.MM.yyyy").create();
 
     public static <T> String toJson(T object){
         return  gson.toJson(object);
