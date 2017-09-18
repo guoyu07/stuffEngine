a50620991506e21fc94d3ea30c1a8fc175d41b18
oprimolenny <oprimolenny@tsconsulting.ru>
2017-09-18 12:16:02 +0300

    добавлен class CertificateService, часть методов не реализована

---
+++ src/main/java/ru/technoserv/services/impl/CertificateServiceImpl.java
@@ -0,0 +1,45 @@
+package ru.technoserv.services.impl;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import ru.technoserv.dao.CertificateDao;
+import ru.technoserv.domain.Certificate;
+import ru.technoserv.services.CertificateService;
+
+import java.util.List;
+
+public class CertificateServiceImpl implements CertificateService {
+
+    private static final Logger logger = Logger.getLogger(CertificateServiceImpl.class);
+
+    @Autowired
+    CertificateDao certificateDao;
+
+    @Override
+    public void create(Certificate certificate) {
+        logger.info("Создаем сертификат");
+        certificateDao.create(certificate);
+    }
+
+    @Override
+    public Certificate readCertByNum(int certNum) {
+        logger.info("Читаем сертификат");
+        Certificate certificate = certificateDao.readCertByNum(certNum);
+        return certificate;
+    }
+
+    @Override
+    public List<Certificate> readAllCertsByEmpID(int empID) {
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public void deleteCertByNum(int certId) {
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public void deleteAllCertsByEmpID(int empID) {
+        throw new RuntimeException("Not implemented");
+    }
+}
