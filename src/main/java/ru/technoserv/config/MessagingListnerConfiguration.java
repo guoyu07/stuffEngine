311b2d1d78c2392442124e3f577a80c49123e8c9
dmitry <witerium@gmail.com>
2017-09-26 14:33:55 +0300

    Добавлена конфигурация для jms

---
+++ src/main/java/ru/technoserv/config/MessagingListnerConfiguration.java
@@ -0,0 +1,29 @@
+package ru.technoserv.config;
+
+import javax.jms.ConnectionFactory;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@EnableJms
+@ComponentScan("ru")
+public class MessagingListnerConfiguration {
+
+    @Autowired
+    ConnectionFactory connectionFactory;
+
+    @Bean
+    public DefaultJmsListenerContainerFactory jmsListenerContainerFactory() {
+        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setConcurrency("1-1");
+        return factory;
+    }
+
+}
