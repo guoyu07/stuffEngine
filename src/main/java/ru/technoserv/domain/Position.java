06c9fc05e315115583b7dbe04a25151670e9ab16
AMaiyndy <aratmay@gmail.com>
2017-10-30 18:23:24 +0300

    В классе Position добавлены новые конструкторы, переопределены equals, hashCode

--- src/main/java/ru/technoserv/domain/Position.java
+++ src/main/java/ru/technoserv/domain/Position.java
@@ -10,7 +10,7 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlType;
 
 @Entity
-@Table(name="POSITION")
+@Table(name = "POSITION")
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "position", propOrder = {
         "id",
@@ -23,7 +23,7 @@ public class Position {
     private String title;
 
     @Id
-    @Column(name="POS_ID")
+    @Column(name = "POS_ID")
     public int getId() {
         return id;
     }
@@ -31,7 +31,8 @@ public class Position {
     public void setId(int id) {
         this.id = id;
     }
-    @Column(name="TITLE")
+
+    @Column(name = "TITLE")
     public String getTitle() {
         return title;
     }
@@ -40,11 +41,41 @@ public class Position {
         this.title = title;
     }
 
+    public Position(int id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public Position() {
+    }
+
+    public Position(int id) {
+        this(id, null);
+    }
+
     @Override
     public String toString() {
         return "Position{" +
                 "id=" + id +
                 ", title='" + title + '\'' +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Position)) return false;
+
+        Position position = (Position) o;
+
+        if (getId() != position.getId()) return false;
+        return getTitle().equals(position.getTitle());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getId();
+        result = 31 * result + getTitle().hashCode();
+        return result;
     }
 }
